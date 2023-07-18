640b6d2f65d7c64144bdf079e2e898cc3b5341b3
Nazar Koltsun <nazar-koltsun@ukr.net>
2023-07-18 16:33:35 +0300

    Added NewMeetupPage

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,1 +1,8 @@
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
+const NewMeetupPage = () => {
+  const onAddMeetupHandler = (meetData) => console.log(meetData);
+  return <NewMeetupForm onAddMeetup={onAddMeetupHandler} />
+}
+
+export default NewMeetupPage;
