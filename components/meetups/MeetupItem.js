ec41db93108ed177a3bf29e9a9565131699c1da0
Nazar Koltsun <nazar-koltsun@ukr.net>
2023-07-20 14:59:49 +0300

    Fixes

--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -1,7 +1,14 @@
 import Card from '../ui/Card';
+import { useRouter } from 'next/router';
 import classes from './MeetupItem.module.css';
 
 function MeetupItem(props) {
+  const router = useRouter();
+
+  const navToDetailPage = () => {
+    router.push(`/${props.id}`)
+  }
+  
   return (
     <li className={classes.item}>
       <Card>
@@ -13,7 +20,7 @@ function MeetupItem(props) {
           <address>{props.address}</address>
         </div>
         <div className={classes.actions}>
-          <button>Show Details</button>
+          <button onClick={navToDetailPage}>Show Details</button>
         </div>
       </Card>
     </li>
---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+const MeetupDetailPage = () => {
+  return (
+    <MeetupDetail
+      imgUrl="https://s.dou.ua/storage-files/how-to-front-end.jpg"
+      title="Frontend meet"
+      address="Lviv, Struyska 22"
+      description="The meetup description"
+    />
+  );
+};
+
+export default MeetupDetailPage;
