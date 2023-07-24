dce48350138d3f21f1844c487faf0fd113435ff9
Nazar Koltsun <nazar-koltsun@ukr.net>
2023-07-24 15:34:45 +0300

    DYMMY_MEETUPS data fixes

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,19 @@
 import MeetupList from '../components/meetups/MeetupList.js';
 
-const DYMMY_MEETUPS = [
+export const DYMMY_MEETUPS = [
   {
     id: 'm1',
     title: 'Frontend meet',
     image: 'https://s.dou.ua/storage-files/how-to-front-end.jpg',
     address: 'Lviv, Struyska 22',
+    description: "The meetup description"
   },
   {
     id: 'm2',
     title: 'Backend meet',
     image: 'https://s3.studytonight.com/curious/uploads/pictures/1631204081-106730.png',
     address: 'Ternopil, Russka 23',
+    description: "The meetup description"
   },
 ];
 
