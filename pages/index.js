d5e169ad67b4032558c5b0cdaec79ce827a21660
Nazar Koltsun <nazar-koltsun@ukr.net>
2023-07-25 19:39:52 +0300

    Fetched meetups from MongoDb

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,30 @@
-import { useEffect } from 'react';
+import { MongoClient } from 'mongodb';
 import MeetupList from '../components/meetups/MeetupList.js';
 
-export const DYMMY_MEETUPS = [
-  {
-    id: 'm1',
-    title: 'Frontend meet',
-    image: 'https://s.dou.ua/storage-files/how-to-front-end.jpg',
-    address: 'Lviv, Struyska 22',
-    description: "The meetup description"
-  },
-  {
-    id: 'm2',
-    title: 'Backend meet',
-    image: 'https://s3.studytonight.com/curious/uploads/pictures/1631204081-106730.png',
-    address: 'Ternopil, Russka 23',
-    description: "The meetup description"
-  },
-];
-
-const HomePage = ({meetups}) => {
+const HomePage = ({ meetups }) => {
   return <MeetupList meetups={meetups} />;
 };
 
 export async function getStaticProps() {
-  const meetups = DYMMY_MEETUPS;
+  const client = await MongoClient.connect(process.env.MONGODB_URI);
+  const db = client.db();
+
+  const meetupsConnections = db.collection('meetups');
+
+  const meetupsData = await meetupsConnections.find().toArray();
   
-  return { 
+  const meetups = meetupsData.map(meetup => ({
+    title: meetup.title,
+    address: meetup.address,
+    image: meetup.image,
+    id: meetup._id.toString()
+  }));
+
+  client.close();
+
+  return {
     props: { meetups },
   };
 }
 
 export default HomePage;
-
